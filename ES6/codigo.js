e8147e892f1fdab1ba5f62201dd93594cc7478e6
unknown <saraeolivera@gmail.com>
2022-08-02 11:57:13 -0300

    up to excercise #10

--- ES6/codigo.js
+++ ES6/codigo.js
@@ -10,6 +10,8 @@ function checkScope() {
   return i;
 }
 
+//------------------------------------------------------------
+
 // #2 An array is declared as const s = [5, 7, 2]. Change the array to [2, 5, 7] using various element assignments.
 
 const s = [5, 7, 2];
@@ -23,6 +25,8 @@ s[2] = 7;
   // Only change code above this line
 }
 editInPlace();
+
+//---------------------------------------------------------
 
 // #3 In this challenge you are going to use Object.freeze to prevent mathematical constants from changing. You need to freeze the MATH_CONSTANTS object so that no one is able to alter the value of PI, add, or delete properties.
 
@@ -44,4 +48,73 @@ function freezeObj() {
 const PI = freezeObj();
 console.log(PI);
 
-// #4 
+//-------------------------------------------------------------
+
+// #4 Rewrite the function assigned to the variable magic which returns a new Date() to use arrow function syntax. Also, make sure nothing is defined using the keyword var.
+
+const magic = () => new Date();
+
+//------------------------------------------------------------
+
+// #5 Rewrite the myConcat function which appends contents of arr2 to arr1 so that the function uses arrow function syntax.
+
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+//----------------------------------------------------
+
+// #6 Modify the function increment by adding default parameters so that it will add 1 to number if value is not specified.
+
+const increment = (number, value = 1 ) => number + value;
+
+//------------------------------------------------------------
+
+// #7 Modify the function sum using the rest parameter in such a way that the function sum is able to take any number of arguments and return their sum.
+
+const sum = (...args) => {
+  let suma = 0;
+    for (let i of args){
+      suma+= i;
+    }
+    return suma;
+  }
+
+  //-------------------------------------------------
+
+// #8 Copy all contents of arr1 into another array arr2 using the spread operator.
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  
+
+console.log(arr2);
+
+//------------------------------------------------------
+
+// #9 Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables today and tomorrow the values of today and tomorrow from the HIGH_TEMPERATURES object.
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+const { today } = HIGH_TEMPERATURES;
+const { tomorrow } = HIGH_TEMPERATURES;
+
+//-------------------------------------------------------
+
+// #10 Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables highToday and highTomorrow the values of today and tomorrow from the HIGH_TEMPERATURES object.
+
+const HIGH_TEMPERATURES1 = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+
+const {today: highToday} = HIGH_TEMPERATURES1;
+const {tomorrow: highTomorrow } = HIGH_TEMPERATURES1; 
+
