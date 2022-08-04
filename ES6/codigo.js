53667b8452c6c84e6654e3f4e34323a42f2defa5
unknown <saraeolivera@gmail.com>
2022-08-03 21:39:45 -0300

    Up to excercise 14

--- ES6/codigo.js
+++ ES6/codigo.js
@@ -118,3 +118,57 @@ const HIGH_TEMPERATURES1 = {
 const {today: highToday} = HIGH_TEMPERATURES1;
 const {tomorrow: highTomorrow } = HIGH_TEMPERATURES1; 
 
+//----------------------------------------------------------------
+
+// #11 Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables lowToday and highToday1 the values of today.low and today.high from the LOCAL_FORECAST object.
+
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+
+  
+const { today: { low: lowToday, high: highToday1 } } = LOCAL_FORECAST;
+
+//----------------------------------------------------------------------
+
+// #12 Use destructuring assignment to swap the values of a and b so that a receives the value stored in b, and b receives the value stored in a.
+
+let a = 8, b = 6;
+// Only change code below this line
+[a, b] = [b, a];
+console.log(a,b);
+
+//-------------------------------------------------------------------------
+
+// #13 Use destructuring assignment with the rest parameter to perform an effective Array.prototype.slice() so that arr is a sub-array of the original array source with the first two elements omitted.
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  // Only change code below this line
+  const [a,b,...arr] = list; // Change this line
+  // Only change code above this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
+
+//---------------------------------------------------------------------------
+
+// #14 Use destructuring assignment within the argument to the function half to send only max and min inside the function.
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+const half = ({min, max}) => (max + min) / 2.0; 
+
+//--------------------------------------------------------------
+
+// #15
+
