2b2072a976b67aac5d9be658920feb74ca255acf
unknown <saraeolivera@gmail.com>
2022-08-05 06:34:53 -0300

    up to excersice #29

--- ES6/codigo.js
+++ ES6/codigo.js
@@ -307,3 +307,73 @@ export default function (x, y) {
 
 //------------------------------------------------------------------------
 
+// #25 In the following code, import the default export from the math_functions.js file, found in the same directory as this file. Give the import the name subtract.
+
+import subtract from "./math_functions.js";
+// Cambia solo el código encima de esta línea
+
+subtract(7,4);
+
+//-------------------------------------------------------------------------
+
+// #26 Create a new promise called makeServerRequest. Pass in a function with resolve and reject parameters to the constructor.
+
+const makeServerRequest = new Promise((resolve , reject ) => {
+
+});
+
+//--------------------------------------------------------------------------
+
+// #27 Make the promise handle success and failure. If responseFromServer is true, call the resolve method to successfully complete the promise. Pass resolve a string with the value We got the data. If responseFromServer is false, use the reject method instead and pass it the string: Data not received.
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer representa una respuesta de un servidor
+  let responseFromServer;
+
+  if(responseFromServer) {
+    resolve('We got the data');
+  } else {  
+    reject('Data not received');
+  }
+});
+
+//-------------------------------------------------------------------------
+
+// #28 Add the then method to your promise. Use result as the parameter of its callback function and log result to the console.
+
+const makeServerRequest = new Promise((resolve, reject) => {
+   // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer = true;
+
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+
+  makeServerRequest.then(result => {
+
+});
+
+console.log(result);
+});
+
+//-------------------------------------------------------------------------
+
+// #29 Add the catch method to your promise. Use error as the parameter of its callback function and log error to the console.
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to false to represent an unsuccessful response from a server
+  let responseFromServer = false;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+
+makeServerRequest.catch(error => {
+  console.log(error);
+});
+
